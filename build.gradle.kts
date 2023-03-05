2c3dfaa0656115799fcde16b971360e6a9bd2aff
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-03-05 09:10:26 +0400

    Refactor repository test case

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import com.google.protobuf.gradle.protobuf
 
+
 plugins {
     java
     idea
--- src/test/java/com/jds/neo4j/reactive/neo4jreactive/repository/TradeRepositoryTest.java
+++ src/test/java/com/jds/neo4j/reactive/neo4jreactive/repository/TradeRepositoryTest.java
@@ -4,57 +4,128 @@ import com.jds.neo4j.reactive.graphs.model.ExchangeNode;
 import com.jds.neo4j.reactive.graphs.model.TradeNode;
 import com.jds.neo4j.reactive.model.TradeProto;
 import com.jds.neo4j.reactive.repository.TradeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.harness.ServerControls;
-import org.neo4j.harness.TestServerBuilders;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.Neo4jContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
-@Testcontainers
+@DataNeo4jTest
+@Transactional(propagation = Propagation.NEVER)
+@ContextConfiguration(initializers = TradeRepositoryTest.TestContainerInitializer.class)
+@Slf4j
 class TradeRepositoryTest {
-    @Container
-    private static final Neo4jContainer<?> neo4j = new Neo4jContainer<>("neo4j:5.5")
-            .withAdminPassword(null); // use no password for testing purposes
-    private static ServerControls serverControls;
-    private final Driver driver;
-    TradeRepository tradeRepository;
 
-    public TradeRepositoryTest() {
-        serverControls = TestServerBuilders.newInProcessBuilder()
-                .withExtension(
-                        "/graphql", "com.graphqlneo4j.graphqlneo4j.GraphQLExtensionFactory")
-                .newServer();
+    @Autowired
+    private TradeRepository tradeRepository;
 
-        driver = GraphDatabase.driver(serverControls.boltURI(),
-                AuthTokens.basic("neo4j", "neo4j"));
+    @Test
+    void testCreateTrade() {
+        TradeNode tradeNode = createTradeNode("AAPL", 100.0, 10L, TradeProto.Side.BUY);
+        tradeRepository.save(tradeNode).block();
+
+        Flux<TradeNode> trades = tradeRepository.findBySymbol("AAPL");
+        StepVerifier.create(trades)
+                .expectNextMatches(trade -> trade.getSymbol().equals("AAPL"))
+                .expectComplete()
+                .verify();
+
+        tradeRepository.delete(tradeNode).block();
     }
 
     @Test
-    void saveTrade_shouldSaveAndRetrieveTrade() {
+    void testUpdateTrade() {
+        TradeNode tradeNode = createTradeNode("AAPL", 100.0, 10L, TradeProto.Side.BUY);
+        tradeRepository.save(tradeNode).block();
+
+        Flux<TradeNode> trades = tradeRepository.findBySymbol("AAPL");
+        StepVerifier.create(trades)
+                .expectNextMatches(trade -> trade.getSymbol().equals("AAPL"))
+                .expectComplete()
+                .verify();
+
+        tradeNode.setQuantity(20L);
+        tradeRepository.save(tradeNode).block();
+
+        Flux<TradeNode> updatedTrades = tradeRepository.findBySymbol("AAPL");
+        StepVerifier.create(updatedTrades)
+                .expectNextMatches(trade -> trade.getQuantity().equals(20L))
+                .expectComplete()
+                .verify();
+
+        tradeRepository.delete(tradeNode).block();
+    }
+
+    @Test
+    void testDeleteTrade() {
+        TradeNode tradeNode = createTradeNode("AAPL", 100.0, 10L, TradeProto.Side.BUY);
+        tradeRepository.save(tradeNode).block();
+
+        tradeRepository.delete(tradeNode).block();
+
+        Flux<TradeNode> trades = tradeRepository.findBySymbol("AAPL");
+        StepVerifier.create(trades)
+                .expectNextCount(0)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testFindTrade() {
+        TradeNode tradeNode = createTradeNode("AAPL", 100.0, 10L, TradeProto.Side.BUY);
+        tradeRepository.save(tradeNode).block();
+
+        Mono<TradeNode> foundTrade = tradeRepository.findById(tradeNode.getId());
+        StepVerifier.create(foundTrade)
+                .expectNextMatches(trade -> trade.getSymbol().equals("AAPL"))
+                .expectComplete()
+                .verify();
+        
+        tradeRepository.delete(tradeNode).block();
+    }
+
+    private TradeNode createTradeNode(String symbol, Double price, Long quantity, TradeProto.Side side) {
         ExchangeNode exchangeNode = new ExchangeNode();
         exchangeNode.setCode("NASDAQ");
 
-        TradeNode tradeNode = new TradeNode(1L, "AAPL", 100.0, 10L, TradeProto.Side.BUY, exchangeNode, System.currentTimeMillis());
-        StepVerifier.create(tradeRepository.save(tradeNode))
-                .expectNextMatches(savedTrade -> savedTrade.getId() != null)
-                .verifyComplete();
-
-        List<TradeNode> trades = tradeRepository.findBySymbol("ABC").collectList().block();
-
-        assertThat(trades).hasSize(1);
-        assertThat(trades.get(0)).isEqualTo(tradeNode);
+        TradeNode tradeNode = new TradeNode();
+        tradeNode.setSymbol(symbol);
+        tradeNode.setPrice(price);
+        tradeNode.setQuantity(quantity);
+        tradeNode.setSide(side);
+        tradeNode.setExchange(exchangeNode);
+        tradeNode.setTimestamp(System.currentTimeMillis());
+        return tradeNode;
     }
+
+    static class TestContainerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:4.0").withoutAuthentication();
+            neo4jContainer.start();
+            configurableApplicationContext
+                    .addApplicationListener((ApplicationListener<ContextClosedEvent>) event -> neo4jContainer.stop());
+            TestPropertyValues
+                    .of(
+                            "spring.neo4j.uri=" + neo4jContainer.getBoltUrl(),
+                            "spring.neo4j.authentication.username=neo4j",
+                            "spring.neo4j.authentication.password=" + neo4jContainer.getAdminPassword()
+                    )
+                    .applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
 }
 
 
